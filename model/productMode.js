c365d6eb720ec60c50a86f2fafd85e9314a86f93
sunil <logins697@gmail.com>
2023-11-26 15:22:09 +0545

    minor code added in product model

--- model/productMode.js
+++ model/productMode.js
@@ -1,5 +1,9 @@
 const mongoose = require ("mongoose");
 const prodcutSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: mongoose.Types.ObjectId, // Automatically generate ObjectId if not provided (this is code not require just added for working if i host this webite on render / vercel/ netfily)
+      },
     name : {
         type : String,
         required : true,
